c525f0fa8eae02ac042b15f8afe7cac8ec366453
Orbmancer <damien.bry@gmail.com>
2015-05-16 08:58:15 +0200

    continued to debug the motherfucka clockpicka

--- js/controllers.js
+++ js/controllers.js
@@ -13,12 +13,47 @@ angular.module('starter.controllers', [])
         $scope.initSlider = function () {
             $(function () {
                 var clock = $('.clockpicker');
-                clock.clockpicker({
-                    autoclose: true
-                });
 
                 console.log("here man");
-                clock.clockpicker('show').clockpicker('toggleView', 'minutes');
+                //clock.clockpicker('show').clockpicker('toggleView', 'minutes');
+                clock.clockpicker({
+                    donetext: 'Done',
+                    init: function() {
+                        console.log("colorpicker initiated");
+                    },
+                    beforeShow: function() {
+                        console.log("before show");
+                    },
+                    afterShow: function() {
+                        console.log("after show");
+                    },
+                    beforeHide: function() {
+                        console.log("before hide");
+                    },
+                    afterHide: function() {
+                        console.log("after hide");
+                    },
+                    beforeHourSelect: function() {
+                        console.log("before hour selected");
+                    },
+                    afterHourSelect: function() {
+                        console.log("after hour selected");
+                    },
+                    beforeDone: function() {
+                        console.log("before done");
+                    },
+                    afterDone: function() {
+                        console.log("after done");
+                    }
+                });
+
+                clock.click(function(e){
+                    // Have to stop propagation here
+                    e.stopPropagation();
+                    console.log('click');
+                    clock.clockpicker('show')
+                        .clockpicker('toggleView', 'minutes');
+                });
             });
         };
 
