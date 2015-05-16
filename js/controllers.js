eccaac0c6abeb579a408c324860a22544f6f55cb
Jean Duthon <duthon.jean@gmail.com>
2015-05-17 01:21:38 +0200

    Leaflet view

--- js/controllers.js
+++ js/controllers.js
@@ -38,6 +38,12 @@ angular.module('starter.controllers', [])
             }
         });
 
+        console.log($("#menuMap").height());
+        console.log($("#blurred"));
+        $("#blurred").height($("#menuMap").height());
+        $("#blurred").height(24);
+        console.log($("#blurred").height());
+
         /**
          * Once state loaded, get put map on scope.
          */
@@ -46,13 +52,18 @@ angular.module('starter.controllers', [])
             $scope.commerce = {
                 'name': 'MachinTruc',
                 'longitude': 2.815848,
-                'latitude': 49.412262
+                'latitude': 49.412262,
+                'ville' : "Compiégne",
+                'adresse' : "49 Rue Notre Dame de Bon Secours",
+                'type' : "Supermarché",
+                'attente' : 10,
+                'distance' : "6.5Km"
             };
 
             $scope.map = {
                 defaults: {
                     tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-                    maxZoom: 18,
+                    maxZoom: 22,
                     zoomControlPosition: 'bottomleft'
                 },
                 markers: {},
@@ -67,8 +78,26 @@ angular.module('starter.controllers', [])
             $scope.map.center = {
                 lat: $scope.commerce.latitude,
                 lng: $scope.commerce.longitude,
-                zoom: 14
+                zoom: 18
             };
+
+
+
+            /*
+
+            var svg = SVG('menuMap').size($("body").width(),(20*$(document).height())/100);
+            svg.attr({fill : "white", 'fill-opacity': "0.5"});
+            var menuMap = $("#menuMap");
+            var yPointSVG = 10;
+            svg.text($scope.commerce.adresse + ",").attr({y : yPointSVG, x: 5,fill : "black"});
+            svg.text($scope.commerce.ville).attr({y: parseInt(yPointSVG) + 25, x: 5, fill: "black"});
+            var midPointSVG = menuMap.width()/2;
+            svg.text($scope.commerce.type).attr({y:yPointSVG,x:midPointSVG,fill:"black","text-anchor":"middle","font-weight":"bold"});
+            svg.text("Q " + $scope.commerce.attente + "'").attr({x:midPointSVG,y:parseInt(yPointSVG) + 25,fill:"black","text-anchor":"middle"});
+            var xPointSVG = menuMap.width() - 10;
+            svg.text($scope.commerce.distance).attr({x:xPointSVG,y:yPointSVG,fill:"black","text-anchor":"end"});
+
+            menuMap.css("z-index","10");*/
         });
 
 
