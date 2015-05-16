e66533e6d81887471ef643e9ea77aff2e07a03b0
Jean Duthon <duthon.jean@gmail.com>
2015-05-16 19:30:18 +0200

    MapView

--- js/controllers.js
+++ js/controllers.js
@@ -31,6 +31,51 @@ angular.module('starter.controllers', [])
     ];
 })
 
+    .controller('mapViewController', function($scope){
+        angular.extend($scope, {
+            defaults: {
+                scrollWheelZoom: false
+            }
+        });
+
+        /**
+         * Once state loaded, get put map on scope.
+         */
+        $scope.$on("$stateChangeSuccess", function() {
+            console.log("TAMER");
+            $scope.commerce = {
+                'name': 'MachinTruc',
+                'longitude': 2.815848,
+                'latitude': 49.412262
+            };
+
+            $scope.map = {
+                defaults: {
+                    tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+                    maxZoom: 18,
+                    zoomControlPosition: 'bottomleft'
+                },
+                markers: {},
+                events: {
+                    map: {
+                        enable: ['context'],
+                        logic: 'emit'
+                    }
+                }
+            };
+
+            $scope.map.center = {
+                lat: $scope.commerce.latitude,
+                lng: $scope.commerce.longitude,
+                zoom: 14
+            };
+        });
+
+
+
+
+    })
+
 .controller('RateController', function($scope, $idPlace) {
 
 })
